4066c3e2014a6a420f11acfb0678af83bfc8a6ac
rigyel-dev <152839920+rigyel-dev@users.noreply.github.com>
2024-12-08 19:28:59 +0600

    MTSF03:Implemeneted Login and Dashboard (#3)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
     {path:'', component: SignupComponent},
     {path:'signup', component: SignupComponent},
-    {path:'login', component: LoginComponent}
+    {path:'login', component: LoginComponent},
+    {path:'dashboard', component: DashboardComponent },
 ];
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,68 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { LoginModel } from '../models/login.model';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
+import { LoginService } from '../services/login.service';
+import { RouterLink, RouterModule } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
+  standalone: true,
+  imports: [RouterLink,RouterModule,ReactiveFormsModule, CommonModule,ToastrModule]
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  submitted = false;
+  errorMessage = '';
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private loginService: LoginService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [
+        Validators.required, 
+        Validators.email
+      ]],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(8),
+      ]]
+    }, { 
+    });
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    const loginData: LoginModel = {
+      email: this.loginForm.value.email,
+      password: this.loginForm.value.password
+    };
+
+    this.loginService.login(loginData).subscribe({
+      next: (response) => {
+        this.toastr.success('Login successful', 'Success');
+        this.router.navigate(['/dashboard']);
+      },
+      error: (data) => {
+        this.toastr.error(data.error?.message || 'Login failed', 'Error');
+      }
+    });
+  }
+
+  // Getter for easy access to form fields
+  get f() { return this.loginForm.controls; }
 }
