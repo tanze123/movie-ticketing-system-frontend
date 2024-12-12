eeb84cdba0271cff261145fc50c2ad70d2205a63
Tshe-Ree <dorjitashi113@gmail.com>
2024-12-12 19:09:08 +0600

    MTSF07: Implement access token storage using local storage.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,18 @@
 // app.config.ts
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
 import { routes } from './app.routes';
+import { authInterceptor } from './interceptor/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(
+      withInterceptors([authInterceptor])
+    ),
     provideAnimations(), // Crucial for animations
     provideToastr({
       timeOut: 3000,
